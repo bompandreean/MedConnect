d10013944b66591da7f2a436a53abb7755a86b75
Andreea Bompa <bompandreean@yahoo.com>
2020-12-10 16:52:35 +0200

    datasource & mySql connections

---
+++ src/main/java/com/example/HealthcareConnect/datasource/Role.java
@@ -0,0 +1,40 @@
+package com.example.HealthcareConnect.datasource;
+
+import javax.persistence.*;
+
+@Entity(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "role")
+    private String role; // doctor, patience, admin
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/example/HealthcareConnect/datasource/User.java
@@ -0,0 +1,75 @@
+package com.example.HealthcareConnect.datasource;
+
+
+import javax.persistence.*;
+
+@Entity(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="role")
+    private Role role;
+
+    @Column(name="cnp")
+    private String cnp;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
+}

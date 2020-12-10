61054e679836e2b96b9305786360fa56918fe5fd
Andreea Bompa <bompandreean@yahoo.com>
2020-12-10 15:28:51 +0200

    First commit

---
+++ src/test/java/com/example/HealthcareConnect/HealthcareConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthcareConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
